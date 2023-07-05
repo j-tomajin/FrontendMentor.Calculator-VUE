4f3f23fbc16102ef7a451f24f81a05ed563e8521
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-05 22:42:59 +0800

    App Vue component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import Calculator from './components/Calculator.vue';
+  import CalcHeader from './components/CalcHeader.vue';
+  
+  export default {
+    name: "App",
+    components: { Calculator, CalcHeader },
+  }
+</script>
+
+<template>
+  <CalcHeader title="calc" />
+  <main>
+    <Calculator />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use './assets/scss/utilities/' as *;
+  
+</style>
